9a894c87cbf553f391793f443d3ad90b5160284c
AVSY <avsy.yap@gmail.com>
2018-01-23 22:39:25 +0800

    Initial commit.

---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 20:44:38 2018
+
+@author: user
+"""
+
+import fitbit
+import gather_keys_oauth2 as Oauth2
+import pandas as pd 
+import datetime as dt
+
