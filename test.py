c70eff281d63dcb30f152aaf0ca706db4ca5bb51
AVSY <avsy.yap@gmail.com>
2018-02-01 00:19:40 +0800

    Created script to get heart rate data.

--- test.py
+++ test.py
@@ -18,3 +18,30 @@ ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
 REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
 
 auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+
+
+def get_heart_rate(auth2_client, date, granularity='1sec'):
+    """
+    Query intraday time series given date
+    granularity: 1sec or 1min
+    """
+    
+    heart_rate_raw = auth2_client.intraday_time_series('activities/heart', base_date=date, detail_level=granularity)
+
+    time_list = []
+    val_list = []
+    
+    for i in heart_rate_raw['activities-heart-intraday']['dataset']:
+        val_list.append(i['value'])
+        time_list.append(i['time'])
+    
+    heart_rate_df = pd.DataFrame({'Heart Rate':val_list,'Time':time_list})
+    
+    return heart_rate_df
+    
+    
+for i in range(20,31):
+    date_today = '2018-01-' + str(i)
+    heart_rate_df = get_heart_rate(auth2_client, date_today)
+    FILEPATH = './data/' + date_today + '.csv'
+    heart_rate_df.to_csv(FILEPATH)
